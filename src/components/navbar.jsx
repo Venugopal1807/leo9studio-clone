be5cb5608246b45f73425e5933099584e0ba0f1b
venu gopal <kunchepu.venugopal@gmail.com>
2025-09-20 11:53:23 +0530

    updated mobile view

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,7 +25,6 @@ export default function Navbar({ onOpenMenu }) {
           {/* LEFT: logo */}
           <div className="flex items-center gap-3">
             <img src={NavLogo} alt="leo9 logo" className="h-8 w-auto" />
-            <span className="font-bold text-lg hidden sm:inline">leo9</span>
           </div>
 
           {/* CENTER: Desktop Links */}
